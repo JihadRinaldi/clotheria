ef77ee9a014bbdb7bd1b98c9dc0cc81b3997e678
Muhammad Jihad Rinaldi <jihadmjr@gmail.com>
2020-01-02 08:27:07 +0700

    update shopdata, create cart utils for grouping same items in cart reducer

--- src/components/collectionitem/CollectionItem.jsx
+++ src/components/collectionitem/CollectionItem.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./collectionitem.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+import CustomButton from "../custombutton/CustomButton";
+import { addItem } from "../../redux/cart/cart.action";
+
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div
@@ -12,10 +17,18 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
       />
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="price">{price} $</span>
+        <span className="price">${price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        {" "}
+        Add to Cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
