a70a378127aa8a870a12716ac97d7be1a25dec44
Muhammad Jihad Rinaldi <jihadmjr@gmail.com>
2020-01-02 07:45:56 +0700

    add carticon and cartdropdown component using redux for reducer and action

---
+++ src/components/cartdropdown/CartDropdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CustomButton from '../custombutton/CustomButton';
+import './cartdropdown.styles.scss'
+
+const CartDropdown = () => (
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+                <CustomButton>GO TO CHECKOUT</CustomButton>
+            </div>
+        </div>
+);
+
+export default CartDropdown;
