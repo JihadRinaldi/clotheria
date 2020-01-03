4f5b90349ff62f8bfa8c6a5f006864a182bf28ce
Muhammad Jihad Rinaldi <jihadmjr@gmail.com>
2020-01-04 04:54:36 +0700

    Updates

---
+++ precache-manifest.7000c15d7ec6513efe76c14476b91573.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c2d88fd9e3e08ae0319ee142001714",
+    "url": "/clotheria/index.html"
+  },
+  {
+    "revision": "1ec098afaab310185b69",
+    "url": "/clotheria/static/css/main.9a502c84.chunk.css"
+  },
+  {
+    "revision": "c2f184ecf32049131db1",
+    "url": "/clotheria/static/js/2.34e9b713.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/clotheria/static/js/2.34e9b713.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ec098afaab310185b69",
+    "url": "/clotheria/static/js/main.6618c1f8.chunk.js"
+  },
+  {
+    "revision": "cb6d289bbbc41131ee60",
+    "url": "/clotheria/static/js/runtime-main.1d8316a5.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clotheria/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "a56d285025f8799112910d2ff849f098",
+    "url": "/clotheria/static/media/shopping-bag.a56d2850.svg"
+  }
+]);
